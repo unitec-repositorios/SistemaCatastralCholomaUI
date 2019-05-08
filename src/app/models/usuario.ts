a6176623905caccdaf5f66f3ff8b9424c58a6f64
jufergom <jufergom@hotmail.com>
2019-05-08 02:38:21 -0600

    agregado el modelo y el service de los usuarios

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,4 @@
+export class Usuario {
+    nombre: string;
+    password: string;
+}
---
+++ src/app/services/usuarios.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../models/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  constructor(private http: HttpClient) { }
+}
