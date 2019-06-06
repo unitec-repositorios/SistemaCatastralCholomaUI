cb4cd77dcf401237ef6ff4e4ba50e28244d840fe
jufergom <jufergom@hotmail.com>
2019-06-06 15:38:00 -0600

    logout option was added to main component. Also, now if you click on Municipalidad de Choloma text, you will return to the main component

--- src/app/components/main-navbar/main-navbar.component.ts
+++ src/app/components/main-navbar/main-navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-main-navbar',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['login']);
   }
 
 }
