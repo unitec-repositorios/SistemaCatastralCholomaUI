1fd2d33a505c86e1fd2b15e8f3188f4580e80f3b
Diego Valenzuela <valenv.dg@gmail.com>
2019-09-01 22:50:45 -0600

    Added model for Detalles Adicionales.

---
+++ src/app/models/detalles-adicionales.spec.ts
@@ -0,0 +1,7 @@
+import { DetallesAdicionales } from './detalles-adicionales';
+
+describe('DetallesAdicionales', () => {
+  it('should create an instance', () => {
+    expect(new DetallesAdicionales()).toBeTruthy();
+  });
+});
---
+++ src/app/models/detalles-adicionales.ts
@@ -0,0 +1,11 @@
+export class DetallesAdicionales {
+    idDetallesAdicionales: number;
+    codigo: string;
+    codEdificacion: number;
+    area: number;
+    porcentaje: number;
+    costoUnitario: number;
+    comentario: number;
+    valorDetallesAdicionales: number;
+    idClaveCatastral: string;
+}
