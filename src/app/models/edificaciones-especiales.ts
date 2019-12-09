cd666d26ca438dff69870010392ed55412ea2914
Josue <josue.rodriguez1551@gmail.com>
2019-12-09 14:47:49 -0600

    Mas pequeños arreglos...

--- src/app/models/edificaciones-especiales.ts
+++ src/app/models/edificaciones-especiales.ts
@@ -2,7 +2,10 @@ export class EdificacionesEspeciales {
     idedificacionesespeciales: string;
     idDatosComplementarios: string;
     nivel: string;
+    Area1: number;
+    Costo: number;
     
+    //Variables utilizadas para el avaluo de Edificaciones
     area: number;
     uso: number;
     clase: number;
