84d754f4404360e7d5c2d81ca522f210877de5ad
DennisCarcamo <joel.carcamos@laureate.net>
2020-03-24 13:06:59 -0600

    listado de fichas

--- src/app/components/fichas/fichas.component.ts
+++ src/app/components/fichas/fichas.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Empleado} from 'src/app/models/empleado';
 import { EmpleadosService } from '../../services/empleados.service';
+import { MatSort, MatPaginator, MatTableDataSource } from '@angular/material';
+import { FichaCatastral } from 'src/app/models/ficha-catastral';
+import { FichasCatastralesService } from 'src/app/services/fichas-catastrales.service';
 
 @Component({
   selector: 'app-fichas',
@@ -8,10 +11,30 @@ import { EmpleadosService } from '../../services/empleados.service';
   styleUrls: ['./fichas.component.css']
 })
 export class FichasComponent implements OnInit {
+  fichas: any = [];
+  displayedColumns: string[] = ["cocata","identidadPropietario","mapa"];
+  dataSource: MatTableDataSource<FichaCatastral>;
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private fichasCatastralesService: FichasCatastralesService) { }
 
   ngOnInit() {
+     this.fichasCatastralesService.getFichaCatastrales()
+       .subscribe(
+         res => {
+           console.log(res);
+           this.fichas = res;
+           // Assign the data to the data source for the table to render
+           this.dataSource = new MatTableDataSource(this.fichas);
+           this.dataSource.paginator = this.paginator;
+           this.dataSource.sort = this.sort;
+         },
+         err => {
+           console.log(err);
+         }
+       );
   }
 
 }
